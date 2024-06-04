f3f875cc9fbc21c1c1ad81b8d66b416c0132e563
antrungnam <antrungnam1234@gmail.com>
2024-06-04 22:01:42 +0700

    zz

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
     reactStrictMode: true,
     images: {
-        domains: ['image.tmdb.org', 'lh3.googleusercontent.com', 'images.remotePatterns'],
+        domains: ['image.tmdb.org', 'lh3.googleusercontent.com', 'images.remotePatterns', 'images.remotePatterns.com'],
+
     },
     env: {
         API_KEY: process.env.API_KEY,
