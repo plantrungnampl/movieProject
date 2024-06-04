1a516574a56dcc3cf8d42e4c74fa44a9bd88be7c
antrungnam <antrungnam1234@gmail.com>
2024-06-04 22:30:58 +0700

    cryyyyyy

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,61 +1,3 @@
-// import React, { Suspense, useState } from "react";
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from "@/components/ui/carousel";
-// import CarouselItemComponent from "@/components/CarouselItemComponent";
-// import { IItem } from "@/model/types";
-// // import { IResult } from "@/model/types";
-
-// interface IResult {
-//   result: IItem[];
-//   result1: IItem[];
-// }
-
-// export default function HomePage({ result, result1 }: IResult) {
-//   const tabData = [
-//     { value: "Today", result: result1 },
-//     { value: "Thisweek", result: result },
-//   ];
-//   return (
-//     <Tabs defaultValue="Today">
-//       {/* map tap */}
-//       <div className="flex gap-5 items-center mb-3">
-//         <p className="text-xl p-1 rounded">Trending</p>
-//         <TabsList>
-//           {tabData.map((tab) => (
-//             <TabsTrigger key={tab.value} value={tab.value}>
-//               {tab.value}
-//             </TabsTrigger>
-//           ))}
-//         </TabsList>
-//       </div>
-//       {/* map data */}
-//       {tabData.map((tab) => (
-//         <TabsContent key={tab.value} value={tab.value}>
-//           <Carousel>
-//             <CarouselContent className="grid grid-cols-auto-fit-minmax gap-4">
-//               {tab.result.map((item) => (
-//                 <CarouselItem
-//                   key={item.id}
-//                   className="md:basis-1/2 lg:basis-1/3 flex"
-//                 >
-//                   <CarouselItemComponent item={item} />
-//                 </CarouselItem>
-//               ))}
-//             </CarouselContent>
-//             <CarouselPrevious>Previous</CarouselPrevious>
-//             <CarouselNext>Next</CarouselNext>
-//           </Carousel>
-//         </TabsContent>
-//       ))}
-//     </Tabs>
-//   );
-// }
 import React, { Suspense, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
@@ -112,7 +54,7 @@ export default async function HomePage() {
         <TabsContent key={tab.value} value={tab.value}>
           <Carousel>
             <CarouselContent className="grid grid-cols-auto-fit-minmax gap-4">
-              {tab.result.map((item) => (
+              {tab.result.map((item: any) => (
                 <CarouselItem
                   key={item.id}
                   className="md:basis-1/2 lg:basis-1/3 flex"
