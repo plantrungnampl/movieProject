c6c8b847164940b041be76308b752f4507571877
antrungnam <antrungnam1234@gmail.com>
2024-06-05 11:47:24 +0700

    zzzzzz

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    output: 'export',
     reactStrictMode: true,
     images: {
         domains: ['image.tmdb.org', 'lh3.googleusercontent.com', 'images.remotePatterns', 'images.remotePatterns.com'],
