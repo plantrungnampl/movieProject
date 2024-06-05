5314d3cf69d41da442141cde463719ef7aec691c
antrungnam <antrungnam1234@gmail.com>
2024-06-05 11:37:37 +0700

    zzz

---
+++ src/app/search/404.tsx
@@ -0,0 +1,29 @@
+// pages/404.tsx
+"use client";
+
+import { Suspense } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Loading from "@/app/loading";
+
+const Custom404Page = () => {
+  const router = useRouter();
+
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="text-2xl mt-4">Page Not Found</p>
+      <Link href="/">
+        <a className="mt-6 text-blue-500">Go back home</a>
+      </Link>
+    </div>
+  );
+};
+
+const NotFound = () => (
+  <Suspense fallback={<Loading />}>
+    <Custom404Page />
+  </Suspense>
+);
+
+export default NotFound;
