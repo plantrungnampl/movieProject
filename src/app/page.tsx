003b236c40a490d43dbf9463a5c171d70e9ddaed
antrungnam <antrungnam1234@gmail.com>
2024-06-08 21:56:07 +0700

    zzz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,104 +1,3 @@
-// "use client";
-// import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-// import axios from "axios";
-// import { Suspense, useState } from "react";
-// import Loading from "./loading";
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-// import CarouselItemComponent from "@/components/CarouselItemComponent";
-// import { fetchData } from "./api/fetchData";
-
-// const API_KEY = process.env.API_KEY;
-
-// async function Home() {
-//   // const res1 = await axios.get(
-//   //   `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
-//   // );
-//   // const res2 = await axios.get(
-//   //   `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&language=en-US&page=1`
-//   // );
-//   // const [data1, data2] = await Promise.all([res2.data, res1.data]);
-
-//   // if (res1.status !== 200 || res2.status !== 200) {
-//   //   throw new Error(data1.message || data2.message || "Failed to fetch data");
-//   // }
-
-//   // const result1 = data1.results;
-//   // const result2 = data2.results;
-//   // const tabData = [
-//   //   { value: "Today", result: result1 },
-//   //   { value: "Thisweek", result: result2 },
-//   // ];
-//   const tabData = await fetchData();
-//   return (
-//     <>
-//       {/* <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-col-1 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52 "> */}
-//       <div>
-//         <Suspense fallback={<Loading />}>
-//           <Tabs defaultValue="Today">
-//             {/* map tap */}
-//             <div className="flex gap-5 items-center mb-3">
-//               <p className="text-xl p-1 rounded">Trending</p>
-//               <TabsList>
-//                 {tabData.map((tab) => (
-//                   <TabsTrigger key={tab.value} value={tab.value}>
-//                     {tab.value}
-//                   </TabsTrigger>
-//                 ))}
-//               </TabsList>
-//             </div>
-//             {/* map data */}
-//             {tabData.map((tab) => (
-//               <TabsContent key={tab.value} value={tab.value}>
-//                 <div>
-//                   <div className="flex gap-4 overflow-x-auto overflow-y-hidden  max-w-screen-xl snap-x transition-colors  ">
-//                     {tab.result.map((item: any) => (
-//                       <div
-//                         key={item.id}
-//                         className="flex-shrink-0 snap-center w-fit "
-//                       >
-//                         <CarouselItemComponent item={item} />
-//                       </div>
-//                     ))}
-//                   </div>
-//                 </div>
-//               </TabsContent>
-//             ))}
-//           </Tabs>
-//         </Suspense>
-//       </div>
-//       {/* </MaxWidthWrapper> */}
-//     </>
-//   );
-// }
-
-// function LastestTrailer() {
-//   return (
-//     <>
-//       <div>
-//         <div>
-//           <h1>Lastest Trailer</h1>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-// function HomePageWrapper() {
-//   return (
-//     <>
-//       <Suspense fallback={<Loading />}>
-//         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-col-1 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52 ">
-//           <div>
-//             <Home />
-//             <div className="mt-14">
-//               <LastestTrailer />
-//             </div>
-//           </div>
-//         </MaxWidthWrapper>
-//       </Suspense>
-//     </>
-//   );
-// }
-// export default HomePageWrapper;
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import React, { Suspense, useEffect } from "react";
 import Loading from "./loading";
@@ -155,7 +54,7 @@ async function Home() {
 
           {Data.map((tab) => (
             <TabsContent key={tab.value} value={tab.value}>
-              <div>
+              <div className="">
                 <div className="flex gap-4 overflow-x-auto overflow-y-hidden  max-w-screen-xl snap-x transition-colors  ">
                   {tab.result.map((item: any) => (
                     <div
--- src/components/LatestTrailerItem.tsx
+++ src/components/LatestTrailerItem.tsx
@@ -11,27 +11,45 @@ import TrailerDialog from "./TrailerDialog";
 import { FaPlay } from "react-icons/fa";
 
 export default function LatestTrailerItem({ videos }: { videos: any[] }) {
+  const [backgroundImages, setBackgroundImages] = React.useState("");
   return (
-    <div>
-      <div className="flex gap-4 overflow-x-auto overflow-y-hidden max-w-screen-xl snap-x transition-colors">
+    <div
+      className="relative"
+      style={{
+        backgroundImage: `url(${backgroundImages})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        transition: "background-image 0.3s ease-in-out",
+      }}
+    >
+      <div className="flex p-9 gap-4 overflow-x-auto overflow-y-hidden max-w-screen-xl snap-x transition-colors custom-scrollbar ">
         {videos.map((movie: any) => (
-          <div key={movie.id} className="relative min-w-[400px] h-auto">
+          <div
+            key={movie.id}
+            className="relative min-w-[400px] overflow-hidden bg-cover bg-no-repeat h-auto"
+            onMouseEnter={() =>
+              setBackgroundImages(
+                `https://img.youtube.com/vi/${movie.videoKey}/0.jpg`
+              )
+            }
+          >
             {movie.videoKey ? (
               <Dialog>
                 <DialogTrigger asChild>
-                  <div className="cursor-pointer relative">
+                  <div className="cursor-pointer relative transition duration-300 ease-in-out hover:scale-110">
                     <Image
                       src={`https://img.youtube.com/vi/${movie.videoKey}/0.jpg`}
                       alt={movie.title || movie.name}
                       width={400}
                       height={300}
-                      className="rounded-lg object-contain w-full h-full"
+                      className="rounded-3xl object-contain w-full h-full "
                       priority={true}
                       layout="responsive"
                       placeholder="blur"
                       blurDataURL={`https://img.youtube.com/vi/${movie.videoKey}/0.jpg`}
                     />
-                    <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="absolute inset-0 flex items-center justify-center transition duration-300 ease-in-out hover:scale-150">
                       <FaPlay className="text-white text-6xl opacity-75" />
                     </div>
                   </div>
@@ -51,6 +69,23 @@ export default function LatestTrailerItem({ videos }: { videos: any[] }) {
           </div>
         ))}
       </div>
+      <style jsx>{`
+        .custom-scrollbar::-webkit-scrollbar {
+          height: 12px;
+        }
+        .custom-scrollbar::-webkit-scrollbar-track {
+          background: #f1f1f1;
+          border-radius: 10px;
+        }
+        .custom-scrollbar::-webkit-scrollbar-thumb {
+          background: #888;
+          border-radius: 10px;
+          width: 20%;
+        }
+        .custom-scrollbar::-webkit-scrollbar-thumb:hover {
+          background: #555;
+        }
+      `}</style>
     </div>
   );
 }
