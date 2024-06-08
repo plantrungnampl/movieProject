2760020c105cab5518b5f1036e9124a475572967
antrungnam <antrungnam1234@gmail.com>
2024-06-08 13:57:40 +0700

    speed light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Header from "../components/Header";
 import { ThemeProvider } from "@/app/components/theme-provider";
 import { Suspense } from "react";
