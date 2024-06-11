ea789f09e27df4475cd46bdc68b391909f06922a
antrungnam <antrungnam1234@gmail.com>
2024-06-11 15:56:46 +0700

    fix text issua in darkode

--- src/app/tv/airring-today/page.tsx
+++ src/app/tv/airring-today/page.tsx
@@ -3,17 +3,15 @@ import React from "react";
 
 export default function ArringToday() {
   return (
-    <div className="min-h-screen text-black flex flex-col items-center justify-center">
-      <h1 className="text-5xl text-black font-bold mb-8 animate-pulse">
-        Coming Soon
-      </h1>
-      <p className="text-black text-lg mb-8">
+    <div className="min-h-screen  flex flex-col items-center justify-center">
+      <h1 className="text-5xl  font-bold mb-8 animate-pulse">Coming Soon</h1>
+      <p className="text-lg mb-8">
         Were working hard to bring you something amazing. Stay tuned!
       </p>
       <div className="flex items-center justify-center">
         <Link
           href={"/"}
-          className="px-4 py-2 text-black font-semibold bg-yellow-500 rounded-lg shadow-lg hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+          className="px-4 py-2  font-semibold bg-yellow-500 rounded-lg shadow-lg hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
         >
           Back to Home
         </Link>
