01cad0ab339aaa8ec4ec07cd020f3013bd5006c9
antrungnam <antrungnam1234@gmail.com>
2024-06-19 13:56:22 +0700

    fix image

--- next.config.mjs
+++ next.config.mjs
@@ -7,14 +7,6 @@ const nextConfig = {
         missingSuspenseWithCSRBailout: false,
     },
     images: {
-        // remotePatterns: [
-        //     { hostname: 'images.unsplash.com', protocol: "https", port: '' },
-        //     { hostname: 'lh3.googleusercontent.com' },
-        //     { hostname: 'images.remotePatterns.com' },
-        //     { hostname: 'img.youtube.com' },
-        //     { hostname: 'image.tmdb.org' },
-
-        // ],
         domains: [
             'image.tmdb.org',
             'lh3.googleusercontent.com',
@@ -25,7 +17,7 @@ const nextConfig = {
         loader: 'default',
         path: '/_next/image',
 
-        deviceSizes: [320, 420, 768, 1024, 1200,], // Các kích thước thiết bị hỗ trợ
+        deviceSizes: [320, 420, 768, 1024, 1200,],
         imageSizes: [
             320,
             420,
@@ -47,7 +39,7 @@ const nextConfig = {
 
     },
     env: {
-        API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+        API_KEY: process.env.API_KEY,
     },
 };
 
