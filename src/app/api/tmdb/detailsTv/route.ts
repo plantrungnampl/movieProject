3e82f2f1a8ed709e5c08534a0d455e5d771ebb0d
antrungnam <antrungnam1234@gmail.com>
2024-06-25 16:04:09 +0700

    zzzzzzzzz

--- src/app/api/tmdb/detailsTv/route.ts
+++ src/app/api/tmdb/detailsTv/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("id");
   if (!id) {
-    return { error: "ID is missing" };
+    return NextResponse.json({ error: "ID is missing" });
   }
 
   try {
