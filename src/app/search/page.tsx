2dc536f1444915cf3a74ed0ce3ec6f7e64edfeba
antrungnam <antrungnam1234@gmail.com>
2024-07-05 17:40:49 +0700

    fix search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,36 @@
+// import React, { Suspense } from "react";
+// import SearchResults from "@/components/SearchResults";
+
+// export const metadata = {
+//   title: "search",
+//   description: "search movie",
+// };
+
+// const SearchPage = async ({
+//   searchParams,
+// }: {
+//   searchParams: { [key: string]: string };
+// }) => {
+//   const { q: searchValue } = searchParams;
+
+//   const res = await fetch(
+//     ` ${process.env.NEXT_PUBLIC_BASE_URL}/api/tmdb/searchResults?q=${searchValue}`
+//   );
+//   if (!res.ok) {
+//     throw new Error(`Failed to fetch: ${res.statusText}`);
+//   }
+//   const data = await res.json();
+//   const results = data.results;
+
+//   return (
+//     <div>
+//       <SearchResults results={results} searchValue={searchValue} />
+//     </div>
+//   );
+// };
+
+// export default SearchPage;
+// //
 import React, { Suspense } from "react";
 import SearchResults from "@/components/SearchResults";
 
@@ -14,8 +47,19 @@ const SearchPage = async ({
   const { q: searchValue } = searchParams;
 
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/tmdb/searchResults?q=${searchValue}`
+    ` https://movie-project-iygl.vercel.app/api/tmdb/searchResults?q=${searchValue}`
   );
+  if (!res.ok) {
+    throw new Error(`Failed to fetch: ${res.statusText}`);
+  }
+
+  const contentType = res.headers.get("content-type");
+  if (!contentType || !contentType.includes("application/json")) {
+    const text = await res.text();
+    console.error("Received non-JSON response:", text);
+    throw new Error("Received non-JSON response");
+  }
+
   const data = await res.json();
   const results = data.results;
 
